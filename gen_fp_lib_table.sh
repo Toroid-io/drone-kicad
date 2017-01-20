08bbcb15aa98914679e917d3a872b1e8df86014f
Andrés Manelli <andresmanelli@gmail.com>
2017-01-20 00:24:36 -0300

    Fix fp_lib_table [ci skip]

--- gen_fp_lib_table.sh
+++ gen_fp_lib_table.sh
@@ -6,4 +6,4 @@ TABLE=$TABLE_DIR/fp-lib-table
 mkdir -p $TABLE_DIR
 touch $TABLE
 
-ls /usr/share/kicad/footprints/ -1 | awk -F . 'BEGIN{print "(fp-lib-table"} {print "  (lib (name "$1")(type KiCad)(uri \"$(KISYSMOD)/"$1"."$2"\")(options \"\")(descr \"\"))"}END{print ")"}' > $TABLE
+ls /usr/share/kicad/footprints/ -1 | awk -F . 'BEGIN{print "(fp_lib_table"} {print "  (lib (name "$1")(type KiCad)(uri \"$(KISYSMOD)/"$1"."$2"\")(options \"\")(descr \"\"))"}END{print ")"}' > $TABLE
