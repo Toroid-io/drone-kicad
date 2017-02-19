09abf8b79b0970405e9046b34a49c28912e06fab
Andrés Manelli <andresmanelli@gmail.com>
2017-02-19 12:45:32 -0300

    Switch to pcbnew python bindings

--- plugin.go
+++ plugin.go
@@ -8,35 +8,10 @@ import (
 )
 
 const (
-	pythonexec       = "python2"
-	sch_script       = "/bin/ci-scripts/export_schematic.py"
-	bom_script       = "/bin/ci-scripts/export_bom.py"
-	grb_script       = "/bin/ci-scripts/export_grb.py"
-	fplibtablescript = "gen_fp_lib_table.sh"
-
-	// Binary masks - MSBits
-	badh_mask     = 1 << 0
-	fadh_mask     = 1 << 1
-	bpaste_mask   = 1 << 2
-	fpaste_mask   = 1 << 3
-	bsilks_mask   = 1 << 4
-	fsilks_mask   = 1 << 5
-	bmask_mask    = 1 << 6
-	fmask_mask    = 1 << 7
-	dwguser_mask  = 1 << 8
-	cmtsuser_mask = 1 << 9
-	eco1_mask     = 1 << 10
-	eco2_mask     = 1 << 11
-	ecuts_mask    = 1 << 12
-	margin_mask   = 1 << 13
-	bcrtyd_mask   = 1 << 14
-	fcrtyd_mask   = 1 << 15
-	bfab_mask     = 1 << 16
-	ffab_mask     = 1 << 17
-
-	// Binary masks - LSBits
-	fcu_mask = 1 << 3
-	bcu_mask = 1 << 31
+	pythonexec = "python2"
+	sch_script = "/bin/ci-scripts/export_schematic.py"
+	bom_script = "/bin/ci-scripts/export_bom.py"
+	grb_script = "/bin/ci-scripts/export_grb.py"
 )
 
 type (
@@ -54,13 +29,15 @@ type (
 
 	// Gerber defines the options for exporting Gerber files
 	GerberLayers struct {
-		Fcu bool `json:"fcu"`
-		Bcu bool `json:"bcu"`
-		Fm  bool `json:"fm"`
-		Bm  bool `json:"bm"`
-		Fs  bool `json:"fs"`
-		Bs  bool `json:"bs"`
-		Ec  bool `json:"ec"`
+		All      bool `json:"all"`
+		Protel   bool `json:"protel"`
+		Fcu      bool `json:"fcu"`
+		Bcu      bool `json:"bcu"`
+		Fmask    bool `json:"fmask"`
+		Bmask    bool `json:"bmask"`
+		Fsilks   bool `json:"fsilks"`
+		Bsilks   bool `json:"bsilks"`
+		Edgecuts bool `json:"edgecuts"`
 	}
 
 	// Options defines what to generate
@@ -95,9 +72,7 @@ func (p Plugin) Exec() error {
 		cmds = append(cmds, commandBOM(p.Project))
 	}
 	if p.Options.GrbGen {
-		cmds = append(cmds, commandGenFpLibTable())
-		cmds = append(cmds, commandSetGerberLayers(p.Project, p.Options.Grb))
-		cmds = append(cmds, commandGerber(p.Project))
+		cmds = append(cmds, commandGerber(p.Project, p.Options.Grb))
 	}
 
 	// Set env variables
@@ -119,60 +94,49 @@ func (p Plugin) Exec() error {
 	return nil
 }
 
-func commandGenFpLibTable() *exec.Cmd {
+func commandGerber(pjt Project, lyr GerberLayers) *exec.Cmd {
 
-	return exec.Command(
-		fplibtablescript,
-	)
-}
+	var options []string
 
-func commandSetGerberLayers(pjt Project, lyr GerberLayers) *exec.Cmd {
+	options = append(options, "-u", grb_script)
+	options = append(options, "--brd", pjt.Name)
+	options = append(options, "--dir", "CI-BUILD/GBR")
 
-	var layerselection_str string
-	var lyr_lsb uint32 = 0
-	var lyr_msb uint32 = 0
+	if lyr.Protel {
+		options = append(options, "--protel")
+	}
+	if lyr.All {
+		options = append(options, "--all")
+		return exec.Command(
+			pythonexec,
+			options...,
+		)
+	}
 	if lyr.Fcu {
-		lyr_lsb |= fcu_mask
+		options = append(options, "--fcu")
 	}
 	if lyr.Bcu {
-		lyr_lsb |= bcu_mask
+		options = append(options, "--bcu")
 	}
-	if lyr.Fs {
-		lyr_msb |= fsilks_mask
+	if lyr.Fsilks {
+		options = append(options, "--fsilks")
 	}
-	if lyr.Bs {
-		lyr_msb |= bsilks_mask
+	if lyr.Bsilks {
+		options = append(options, "--bsilks")
 	}
-	if lyr.Fm {
-		lyr_msb |= fmask_mask
+	if lyr.Fmask {
+		options = append(options, "--fmask")
 	}
-	if lyr.Bm {
-		lyr_msb |= bmask_mask
+	if lyr.Bmask {
+		options = append(options, "--bmask")
 	}
-
-	layerselection_str = fmt.Sprintf("%#08x_%08x", lyr_msb, lyr_lsb)
-
-	var sed_cmd string
-	sed_cmd = fmt.Sprintf("%s %s%s", "s/\\([\\s\\t]*layerselection\\).*$/\\1", layerselection_str, ")/")
-
-	var brd_name string
-	brd_name = fmt.Sprintf("%s.kicad_pcb", pjt.Name)
-
-	return exec.Command(
-		"sed",
-		"-i",
-		sed_cmd,
-		brd_name,
-	)
-}
-
-func commandGerber(pjt Project) *exec.Cmd {
+	if lyr.Edgecuts {
+		options = append(options, "--gko")
+	}
 
 	return exec.Command(
 		pythonexec,
-		"-u",
-		grb_script,
-		pjt.Name,
+		options...,
 	)
 }
 
