ca5f8b30570a4f24a208d41e781ca30e368eea87
Andrés Manelli <andresmanelli@gmail.com>
2017-02-21 11:15:06 -0300

    Lib tables is set in base docker image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM toroid/kicad
 ADD drone-kicad /bin/
-ADD gen_fp_lib_table.sh /bin/
 ENTRYPOINT /bin/drone-kicad
--- gen_fp_lib_table.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TABLE_DIR=/root/.config/kicad
-TABLE=$TABLE_DIR/fp-lib-table
-
-mkdir -p $TABLE_DIR
-touch $TABLE
-
-ls /usr/share/kicad/footprints/ -1 | awk -F . 'BEGIN{print "(fp_lib_table"} {print "  (lib (name "$1")(type KiCad)(uri \"$(KISYSMOD)/"$1"."$2"\")(options \"\")(descr \"\"))"}END{print ")"}' > $TABLE
